5b8b7b256e49b3726f074e89f4991c7ac8dd7362
walter kiprono <walterkiprono81@gmail.com>
2025-03-27 12:27:11 +0300

    feat: add screenshot details and change it to headless to avoid site blocks

--- index.js
+++ index.js
@@ -1,43 +1,87 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const path = require("path");
-var random = require('random-string-generator');
+const viewports = require("./sizes/viewports.json");
 
 (async () => {
     // Creating a browser instance
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false
+  });
 
   // Creating a new page
   const page = await browser.newPage();
-
-  // Screen sizes ->  https://gs.statcounter.com/screen-resolution-stats/desktop/worldwide
-
-  // Adjusting width and height of the viewport
-  await page.setViewport({ width: 1536, height: 864 });
   
-  let urlLink = 'https://facebook.com';
+  let urlLink = 'https://equityonline.equitygroupholdings.com/';
 
   const url = new URL(urlLink);
 
   // Open URL in current page
-  await page.goto(url.href);
+  await page.goto(url.href, { waitUntil: "networkidle0"});
 
-  // The path to the new directory
-  const dir = path.join("pictures", url.hostname);
+  // The path to the screenshot directory
+  const dir = path.join("screenshots", url.hostname);
 
   // Check if it exists
   if(!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
   }
 
-  const fileName = random("lower") + ".png";
+// // The path to the screencast directory
+// const dir1 = path.join("screencasts", url.hostname);
 
-  const filePath = path.join(dir, fileName)
+// // Check if it exists
+// if(!fs.existsSync(dir1)) {
+//   fs.mkdirSync(dir1);
+// }
 
-  // Capture screenshot
-  await page.screenshot({
-    path: filePath,
+  
+  // Screen sizes ->  https://gs.statcounter.com/screen-resolution-stats/desktop/worldwide
+
+  // const forms = await page.$$('form');
+  // if (forms.length > 0) {
+  //   console.log(`Found ${forms.length} forms on the page.`);
+  //   await page.screenshot({ path: 'form_screenshot.png' });
+  // }
+
+  // Hide scrollbars
+  await page.evaluate(() => {
+    document.body.style.overflowY = 'hidden';
   });
+  
+  const viewportsList = [...viewports.desktop, ...viewports.tablet, ...viewports.mobile]
+  for(v of viewportsList) {
+    let dimensions = v.split("x");
+    await page.setViewport({ width: Number(dimensions[0]), height: Number(dimensions[1])});
+
+    const fileName = v + ".png";
+
+    const filePath = path.join(dir, fileName)
+  
+    // Capture screenshot
+    await page.screenshot({
+      path: filePath,
+      // fullPage: true
+    });
+  };
+  
+
+  // // screencast
+  // const screenCastPath = path.join(dir1, "screencast.webm");
+  // const recorder = await page.screencast({ path: screenCastPath });
+  // await recorder.stop();
+
+  // Adjusting width and height of the viewport
+  // await page.setViewport({ width: 1536, height: 864 });
+
+  // const fileName = "1536x864" + ".png";
+
+  // const filePath = path.join(dir, fileName)
+
+  // // Capture screenshot
+  // await page.screenshot({
+  //   path: filePath,
+  // });
 
   // Close the browser instance
   await browser.close();
